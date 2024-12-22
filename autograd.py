319c46c53bb03c2087b19cbfd8abb9b6b87adadd
Shahain Manujith <86903975+itsShahain@users.noreply.github.com>
2024-12-23 00:43:05 +0530

    Update autograd.py

--- autograd.py
+++ autograd.py
@@ -3,6 +3,7 @@ class Value:
         self.data = data
         self.grad = 0.0
         self._prev = set(_children)
+        self._backward = lambda: None
         self._op = _op
         self.label = label
     
@@ -10,12 +11,89 @@ class Value:
         return f"Value(data={self.data})"
     
     def __add__(self, other):
-        return Value(self.data + other.data, (self, other, "+"))
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), "+")
+        def backward():
+            self.grad += out.grad
+            other.grad += out.grad
+        out._backward = backward
+        return out
+    
+    def __radd__(self, other):
+        return self + other
+    
+    def __neg__(self):
+        return self * -1
+    
+    def __sub__(self, other):
+        return self + (-other)
+    
+    def __rsub__(self, other):
+        return -(self - other)
     
     def __mul__(self, other):
-        return Value(self.data * other.data, (self, other, "*"))
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), "*")
+        def backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = backward
+        return out
+    
+    def __rmul__(self, other):
+        return self * other
+    
+    def __truediv__(self, other):
+        return self * other**-1
+    
+    def __pow__(self, other):
+        assert isinstance(other, (int, float))
+        out = Value(self.data ** other)
+
+        def backward():
+            self.grad += other * self.data ** (other - 1) * out.grad
+        
+        out._backward = backward
+        return out
+
     
     def tanh(self):
         x = self.data
         t = (math.exp(2*x)-1)/(math.exp(2*x)+1)
-        return Value(t, (self, ), "tanh")
+        out = Value(t, (self, ), "tanh")
+        def backward():
+            self.grad += (1 - out.data**2) * out.grad
+        out._backward = backward
+        return out
+    
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self, ), label="exp")
+
+        def backward():
+            self.grad += out.data * out.grad
+
+        out._backward = backward
+        return out
+    
+    def backward(self):
+
+        visited = [self]
+        topo = []
+
+        def dfs(node):
+            visited.append(node)
+            for child in node._prev:
+                if (child not in visited):
+                    dfs(child)
+            topo.append(node)
+
+        dfs(self)
+
+        self.grad = 1
+        topo.reverse()
+        for node in topo:
+            node._backward()
+        
+                
+        
